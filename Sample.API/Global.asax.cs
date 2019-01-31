0f36960c9c4d64568ff3ac73efa6d55ab9ab2221
Dan Williams <dankwilliams1980@gmail.com>
2019-01-31 16:52:37 -0600

    Save project remove files

--- Sample.API/Global.asax.cs
+++ Sample.API/Global.asax.cs
@@ -17,7 +17,6 @@ namespace WebApplication2
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }

3978d238f950924c4bb8eceeb5ef0d9641811afe
Wyomann <dankwilliams1980@gmail.com>
2019-02-01 06:35:26 -0600

    Update home.component.ts

--- Sample.Presentation/ClientApp/src/app/home/home.component.ts
+++ Sample.Presentation/ClientApp/src/app/home/home.component.ts
@@ -11,6 +11,9 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    // Normally I wouldn't hardcode in a url value and I'd inject
+    // an ApiService to do the http calls but for brevity I'm just
+    // calling the hello world API here.
     this.http.get("http://localhost:61390/api/values")
       .subscribe(data => {
         this.responseValue = data;

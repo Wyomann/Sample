ea3ee0b5badf57082db91139738b729e8262cf2f
Wyomann <dankwilliams1980@gmail.com>
2019-02-01 08:21:18 -0600

    Update ValuesController.cs

--- Sample.API/Controllers/ValuesController.cs
+++ Sample.API/Controllers/ValuesController.cs
@@ -26,26 +26,5 @@ namespace WebApplication2.Controllers
         {
             return this.repo.GetData();
         }
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
     }
 }
